ea8bb51fea92f4a43f67b8f8d9983012594b053a
bartoszmaka <moncznybartosz@gmail.com>
2016-08-26 17:39:43 +0200

    cleaned up a little

--- 1_2.rb
+++ 1_2.rb
@@ -1,21 +1,7 @@
-class Floors_walker
-  def initialize()
+class FloorsWalker
+  def initialize(path)
     @floor = 0
-    # line below converts file to array of [char, index] pairs
-    @way = File.open('inputs/1input.txt').read.chars.to_enum.with_index(1).to_a
-  end
-
-  def walk(char)
-    case char
-    when '('
-      @floor += 1
-    when ')'
-      @floor -= 1
-    end
-  end
-
-  def in_basement?
-    @floor == -1 ? true : false
+    @way = unpack(path)
   end
 
   def solve
@@ -24,8 +10,26 @@ class Floors_walker
       return index if in_basement?
     end
   end
+
+private
+
+  def unpack(path)
+    File.open(path).read.chars.to_enum.with_index(1).to_a
+  end
+
+  def walk(char)
+    case char
+    when '(' then @floor += 1
+    when ')' then @floor -= 1
+    end
+  end
+
+  def in_basement?
+    @floor == -1 ? true : false
+  end
+
 end
 
-Santa = Floors_walker.new
+Santa = FloorsWalker.new('inputs/1input.txt')
 puts Santa.solve
 
