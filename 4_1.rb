4c6fa82b74da785105ccb80e0e52f3319cbfb6f9
bartoszmaka <moncznybartosz@gmail.com>
2016-08-31 11:43:35 +0200

    solved problem 4 in ruby

---
+++ 4_1.rb
@@ -0,0 +1,26 @@
+require 'digest'
+include Digest
+
+class CoinsMiner
+
+  attr_reader :number
+
+  def initialize secret_key
+    @key = secret_key
+    @number = 0
+    mine
+  end
+
+  private
+
+  def mine
+    loop do
+      hash = Digest::MD5.hexdigest @key + @number.to_s
+      hash.start_with?("00000") ? break : @number += 1
+    end
+  end
+end
+
+# puts "usage: ruby 4_1.rb input.txt"; abort if ARGV.empty?
+Santa = CoinsMiner.new File.open(ARGV[0]).read.strip
+puts Santa.number
